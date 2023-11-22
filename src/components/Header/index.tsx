db9c389232d04328cfacd400c7fadc1f1089df8d
felipesanderp <felipe.panisson@hotmail.com>
2023-11-22 17:10:54 -0300

    header responsivine

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,8 +2,8 @@ import { MobileNav } from './mobile-nav'
 
 export function Header() {
   return (
-    <header className="sticky mb-12 p-4">
-      <div className="flex items-center justify-between">
+    <header className="sticky mb-12 p-4 lg:flex lg:h-20 lg:items-center lg:shadow-lg">
+      <div className="flex items-center justify-between lg:mx-auto lg:w-full lg:max-w-[1440px]">
         <span>felipesanderp</span>
         <MobileNav />
       </div>
